bbed1077e6cf362e98290bcfec93e8d13ad48c69
th35tupid <148354229+th35tupid@users.noreply.github.com>
2023-10-26 00:52:22 -0400

    Add 10.281A.Word_Capitalization.cpp

---
+++ codeforces/10.281A.Word_Capitalization.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+	{
+		string str;
+		cin >> str;
+		if(((int)str[0])>57)
+		str[0] = toupper(str[0]); 
+
+		cout<< str;
+
+
+
+
+return 0;
+}
